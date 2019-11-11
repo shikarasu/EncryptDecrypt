a0bc1229f7fa0caa7a84cef15308adc78d2a3d5c
Brandon <contact@brandonhunter.co.za>
2019-11-11 15:26:37 +0100

    Add project files.

---
+++ EncryptDecrypt/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace EncryptDecrypt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.txtEncryptText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDecryptText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDecryptResult = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEncryptResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(49, 173);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(226, 48);
+            this.btnEncrypt.TabIndex = 0;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click_1);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(281, 173);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(226, 48);
+            this.btnDecrypt.TabIndex = 1;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click_1);
+            // 
+            // txtEncryptText
+            // 
+            this.txtEncryptText.Location = new System.Drawing.Point(146, 12);
+            this.txtEncryptText.Name = "txtEncryptText";
+            this.txtEncryptText.Size = new System.Drawing.Size(292, 20);
+            this.txtEncryptText.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Encrypt Text:";
+            // 
+            // txtDecryptText
+            // 
+            this.txtDecryptText.Location = new System.Drawing.Point(146, 81);
+            this.txtDecryptText.Name = "txtDecryptText";
+            this.txtDecryptText.Size = new System.Drawing.Size(292, 20);
+            this.txtDecryptText.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Dencrypt Text:";
+            // 
+            // txtDecryptResult
+            // 
+            this.txtDecryptResult.Location = new System.Drawing.Point(147, 106);
+            this.txtDecryptResult.Name = "txtDecryptResult";
+            this.txtDecryptResult.Size = new System.Drawing.Size(292, 20);
+            this.txtDecryptResult.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(90, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Result:";
+            // 
+            // txtEncryptResult
+            // 
+            this.txtEncryptResult.Location = new System.Drawing.Point(146, 38);
+            this.txtEncryptResult.Name = "txtEncryptResult";
+            this.txtEncryptResult.Size = new System.Drawing.Size(292, 20);
+            this.txtEncryptResult.TabIndex = 6;
+            this.txtEncryptResult.TextChanged += new System.EventHandler(this.txtEncryptResult_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 233);
+            this.Controls.Add(this.txtDecryptResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtEncryptResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDecryptText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEncryptText);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Name = "Form1";
+            this.Text = "Encrypt and Decrypt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.TextBox txtEncryptText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDecryptText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDecryptResult;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEncryptResult;
+    }
+}
+
---
+++ EncryptDecrypt/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace EncryptDecrypt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string Encrypt(string a, byte[] k, byte[] i)
+        {
+            string result;
+
+            RijndaelManaged rijM = new RijndaelManaged();
+            using(MemoryStream mStream = new MemoryStream())
+            {
+                using(ICryptoTransform encryptor = rijM.CreateEncryptor(k, i))
+                {
+                    using(CryptoStream cStream = new CryptoStream(mStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using(StreamWriter sWriter = new StreamWriter(cStream))
+                        {
+                            sWriter.Write(a);
+                        }
+                    }
+                }
+                result = System.Convert.ToBase64String(mStream.ToArray());
+            }
+            rijM.Clear();
+            return result;
+        }
+
+        
+        private string Decrypt(string a, byte[] K, byte[] I)
+        {
+            string result;
+            RijndaelManaged rijM = new RijndaelManaged();
+            using (MemoryStream msDecrypt = new MemoryStream(System.Convert.FromBase64String(a)))
+            {
+                using (ICryptoTransform decryptor = rijM.CreateDecryptor(K, I))
+                {
+                    using(CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            result = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            rijM.Clear();
+            return result;
+        }
+
+        private void btnEncrypt_Click_1(object sender, EventArgs e)
+        {
+            byte[] rijK = Encoding.ASCII.GetBytes("abcdefg_abcdefg_abcdefg_abcdefg_");
+            byte[] i = Encoding.ASCII.GetBytes("abcdefg_abcdefg_");
+
+            txtEncryptResult.Text = Encrypt(txtEncryptText.Text, rijK, i);
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnDecrypt_Click_1(object sender, EventArgs e)
+        {
+            byte[] rijK = Encoding.ASCII.GetBytes("abcdefg_abcdefg_abcdefg_abcdefg_");
+            byte[] i = Encoding.ASCII.GetBytes("abcdefg_abcdefg_");
+
+            txtDecryptResult.Text = Decrypt(txtDecryptText.Text, rijK, i);
+        }
+
+        private void txtEncryptResult_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
